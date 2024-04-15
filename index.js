712ea057ed29f9a0a5809b8598fc44aae0e7292a
--add <rahmanaky521@gmail.com>
2024-04-15 19:19:46 +0600

    first commit

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
-app.use(json());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.n6wfj5p.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -133,5 +133,5 @@ app.listen(port, () => {
 });
 
 // export express api
-export default app;
+module.exports = app;
 run().catch(console.dir);
